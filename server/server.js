bd621eb657ef2939f98233c87a8223652910e555
parth kakadiya <parth.kakadiya1208@gmail.com>
2018-06-09 16:18:36 +0200

    Final Commit

--- server/server.js
+++ server/server.js
@@ -1,16 +1,15 @@
 const express = require('express');
-const path = require('path');
 const socketIO = require('socket.io');
 const http = require('http');
 const rn = require('random-number');
 
 const port = process.env.PORT || 5000;
-const publicPath = path.join(__dirname, '../client/src');
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 const cors = require('cors');
 
+// Middleware between both server
 app.options('*',cors());
 
 // method for creating random integer
@@ -29,19 +28,5 @@ io.on('connection', socket => {
         }
         socket.emit("FromAPI", n);
     }, 10000);
-
-});
-// for cross domain connection
-const allowCrossDomain = function(req,res,next) {
-
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'PATCH, OPTIONS,GET,PUT,POST,DELETE,HEAD');
-    res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, X-Auth-Token, Access-Control-Allow-Headers, Accept-Control-Request-*, Origin,Accept, X-Requested-With, Content-Type, Content-Range, Content-Disposition, Content-Description, If-Modified-Since, Access-Control-Request-Method, Access-Control-Request-Headers, api-key');
-    next();
-};
-app.use(allowCrossDomain);
-
-app.get('/api', (req, res) => {
-    res.send({ express: publicPath });
 });
 server.listen(port, () => console.log(`Listening on port ${port}`));
